69b19455e2873fb1838f791854ceed3e75973869
gustavolivera <social.gustavosilva@gmail.com>
2024-09-20 10:04:16 -0300

    Add project files.

---
+++ WebApplication1/Controllers/NotasController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calcular(NotasModel notas)
+        {
+            ModelState.Clear();
+            if (validate(notas))
+            {
+                notas.CalcularMedia();
+                
+            }
+            return View("Index", notas);
+        }
+
+        private bool validate(NotasModel notas) {
+            bool valid = true;
+            if (notas.Nota1 == null) {
+                ModelState.AddModelError("Nota1", "Informe um valor numérico");
+                valid = false;
+            }
+
+            if(notas.Nota1 < 0 || notas.Nota1 > 10)
+            {
+                ModelState.AddModelError("Nota1", "O valor deve estar compreendido entre 0 e 10");
+                valid = false;
+            }
+
+            if (notas.Nota2 == null)
+            {
+                ModelState.AddModelError("Nota2", "Informe um valor numérico");
+                valid = false;
+            }
+
+            if (notas.Nota1 < 0 || notas.Nota1 > 10)
+            {
+                ModelState.AddModelError("Nota2", "O valor deve estar compreendido entre 0 e 10");
+                valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ WebApplication1/Models/NotasModel.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Models
+{
+    public class NotasModel
+    {
+        public float? Nota1 { get; set; }
+
+        public float? Nota2 { get; set; }
+
+        public float? Media { get; set; }
+
+        public void CalcularMedia() { 
+            
+            
+            Media = (Nota1.Value + Nota2.Value)/2;
+            
+
+        }
+
+    }
+}
